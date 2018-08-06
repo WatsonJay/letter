32f922e4927049363a5681c5def0a1a0c5a01cb0
WatsonJay <446725026@qq.com>
2018-08-06 10:19:52 +0800

    updata

---
+++ 表白信/表白信/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace 表白信
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::表白信.Properties.Resources._1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(60, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 197);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("幼圆", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(74, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "同意";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("幼圆", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(443, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "不同意（）";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("幼圆", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(54, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "表白信";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("幼圆", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(279, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("幼圆", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(260, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "退出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 405);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 表白信/表白信/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 表白信
+{
+    public partial class Form1 : Form
+    {
+        bool close = false, move = true;
+        int time = 10;
+        int a = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = 表白信.Properties.Resources._1;
+            label2.Text = "能做我的女朋友吗？⁄(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄";
+            close = false;move = false;
+            time = 10;
+            a = 0;
+            timer1.Start();
+            button2.Text = "不同意(" + time.ToString() + ")";
+            button3.Visible = false;
+            button2.Enabled = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (close == false)
+            {
+                if (MessageBox.Show("你要这么狠心离开我吗？Σ(っ °Д °;)っ", "拒绝回答", MessageBoxButtons.OKCancel) == DialogResult.OK)
+
+                {
+                    if(a >= 3)
+                    {
+                        MessageBox.Show("(°Д°≡°Д°)为什么这么对我.....");
+                        pictureBox1.BackgroundImage = 表白信.Properties.Resources._3;
+                    }
+                    else
+                    {
+                        MessageBox.Show("<(￣3￣)>哼！,你走不了");
+                        a++;
+                    }
+                    e.Cancel = true;
+
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "媳妇，爱你，么么哒(ฅ>ω<*ฅ)";
+            pictureBox1.BackgroundImage = 表白信.Properties.Resources._2;
+            close = true;
+            move = false;
+            button2.Visible = false;
+            timer1.Stop();
+            button1.Visible = false;
+            button3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (move == false && close == true)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            if (move == true&& close == false)
+            {
+                int x = this.Width - button2.Width;
+                int y = this.Height - button2.Height;
+                Random r = new Random();
+                button2.Location = new Point(r.Next(0, x + 1), r.Next(0, y + 1));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (move == true && close == false)
+            {
+                MessageBox.Show("你是按不到我的¦•ˇ₃ˇ•｡)");
+                int x = this.Width - button2.Width;
+                int y = this.Height - button2.Height;
+                Random r = new Random();
+                button2.Location = new Point(r.Next(0, x + 1), r.Next(0, y + 1));
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (--time == 0)
+            {
+                timer1.Stop();
+                move = true;
+                button2.Enabled = true;
+                button2.Text = "不同意";
+            }
+            else
+            {
+                button2.Text = "不同意(" + time.ToString() + ")";
+            }
+        }
+    }
+}
